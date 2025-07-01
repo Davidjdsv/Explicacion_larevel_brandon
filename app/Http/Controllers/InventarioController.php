2c6518e1a10da9eb02e3d0dbcf76bd6747847fdf
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-07-01 15:42:35 -0500

    feat:Creando los métodos del controlador y las rutas en la api

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -17,9 +17,42 @@ class InventarioController extends Controller
     public function show($id){
         $item = Inventario::find($id);
         if($item){
-            return response()->json(['message' => 'Producto no encontrado']);
+            return response()->json(['message' => 'Producto no encontrado'], 404);
         } else {
             return response()->json($item);
         }
     }
+
+    public function destroy($id){
+        $item = Inventario::find($id);
+        if($item){
+            $item->delete();
+            return response()->json(['message' => 'Producto Eliminado']);
+        } else {
+            return response()->json(['message' => 'Producto no encontrado'], 404);
+        }
+    }
+
+    public function store(Request $request){
+        $validate = $request->validate([
+            //Párametros required/nullable/tipo_dato/longitud_string(si es uno)
+            'nombre' => 'required|string|max:255',
+            'categoria' => 'required|string|max:255',
+            'descripcion' => 'required|string|max:255',
+            'precio' => 'required|numeric',
+            'disponible' => 'required|boolean',
+            'fecha_creacion' => 'required|date',
+            'cantidad' => 'required|integer'
+        ]);
+
+        $item = Inventario::create($validate);
+
+        // Retorna mensaje desde la app
+        return response() -> json([
+            'message' => 'Producto creado correctamente',
+            'item' => $item 
+        ], 201); 
+    }
 }
+
+// Ahora nos vamos a la API
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\InventarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/inventario', [InventarioController::class, 'index']);
+Route::post('/inventario', [InventarioController::class, 'store']);
+Route::get('/inventario/{id}', [InventarioController::class, 'show']);
+Route::delete('invetario/{id}', [InventarioController::class, 'destroy']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
