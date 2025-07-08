48c8d59bfeff08efd72d3c98353d94f045de587a
Jhoan David Sinisterra <sinisterravalenciajhoandavid@gmail.com>
2025-07-08 14:35:57 -0500

    Cambiando la ruta en api.php

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\InventarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/inventario', [InventarioController::class, 'index']);
-Route::post('/inventario', [InventarioController::class, 'store']);
-Route::get('/inventario/{id}', [InventarioController::class, 'show']);
-Route::delete('invetario/{id}', [InventarioController::class, 'destroy']);
+Route::get('/productos_angular', [InventarioController::class, 'index']);
+Route::post('/productos_angular', [InventarioController::class, 'store']);
+Route::get('/productos_angular/{id}', [InventarioController::class, 'show']);
+Route::delete('/productos_angular/{id}', [InventarioController::class, 'destroy']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
